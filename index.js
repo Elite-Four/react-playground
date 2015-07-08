7eac7b2a798c185aadb17507efad71823d507d28
George Chung <Gerhut@GMail.com>
2015-07-08 17:25:46 +0800

    simplize index.js

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-var app = require('./app')
-var port = 3000
-console.log('Server listenning on port ' + port)
-app.listen(3000)
+console.log('Server listenning on port 3000')
+require('./app').listen(3000)
