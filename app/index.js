ed332d86eee62e43532678f80c6ee5430104adf8
George Chung <Gerhut@GMail.com>
2015-07-08 17:36:17 +0800

    remove jade

--- app/index.js
+++ app/index.js
@@ -1,14 +1,10 @@
 var express = require('express')
 var morgan = require('morgan')
-var jade = require('jade')
 
 app = express()
 
 app.enable('case sensitive routing')
 app.enable('strict routing')
-
-app.engine('jade', jade.__express)
-app.set('view engine', 'jade')
 
 app.use(morgan('dev'))
 
