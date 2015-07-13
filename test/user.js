b0ac25b5f36eec1628059b34fc10fda52239503a
George Chung <Gerhut@GMail.com>
2015-07-13 21:21:55 +0800

    add user test

---
+++ test/user.js
@@ -0,0 +1,52 @@
+var Buffer = require('buffer').Buffer
+var fs = require('fs')
+var path = require('path')
+
+require('should')
+var mkdirp = require('mkdirp')
+
+var root = require('config').get('code.root')
+
+describe('User', function () {
+  var codename = '-*- dummyCode -*-'
+  var request, dummyUser, pathname
+
+  before(function (done) {
+    request = require('./helpers/request')
+    dummyUser = require('./helpers/dummyUser')
+
+    pathname = path.join(root, dummyUser.id.toString(), 'component')
+    mkdirp(pathname, function (err) {
+      if (err) return done(err)
+
+      pathname = path.join(pathname, new Buffer(codename).toString('hex'))
+      fs.writeFile(pathname, '', done)
+    })
+  })
+
+  it('should be able to catch user info include code list', function (done) {
+    request.get('/@' + dummyUser.username, {
+      json: true
+    }, function (err, response, body) {
+      if (err) return done(err)
+      body.should.eql({ codes: [ codename ] })
+      done()
+    })
+  })
+
+  after(function (done) {
+    fs.unlink(pathname, function (err) {
+      if (err) return done(err)
+
+      pathname = path.dirname(pathname)
+
+      fs.rmdir(pathname, function (err) {
+        if (err) return done(err)
+
+        pathname = path.dirname(pathname)
+
+        fs.rmdir(pathname, done)
+      })
+    })
+  })
+})
